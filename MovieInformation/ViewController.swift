a0b9791a40b284339a57745a1c3432f3d812bcf0
김영채 <kevinkim2586@gmail.com>
2021-03-11 14:00:59 +0900

    Add assets needed for app

---
+++ MovieInformation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieInformation
+//
+//  Created by Kevin Kim on 2021/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
